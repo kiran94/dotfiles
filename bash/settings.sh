44aef83bd9084794d81345cc8bb4a332105e5271
kiran94 <kiran_patel94@hotmail.com>
2020-12-20 16:19:58 +0000

    added XDG env vars

--- bash/settings.sh
+++ bash/settings.sh
@@ -1,5 +1,22 @@
 export EDITOR=vim
 
+# XDG standard environment variables
+# https://github.com/adrg/xdg#default-locations
+export XDG_DATA_HOME=~/.local/share
+export XDG_DATA_DIRS=/usr/local/share
+export XDG_CONFIG_HOME=~/.config
+export XDG_CONFIG_DIRS=/etc/xdg
+export XDG_CACHE_HOME=~/.cache
+
+export XDG_DESKTOP_DIR=~/Desktop
+export XDG_DOWNLOAD_DIR=~/Downloads
+export XDG_DOCUMENTS_DIR=~/Documents
+export XDG_MUSIC_DIR=~/Music
+export XDG_PICTURES_DIR=~/Pictures
+export XDG_VIDEOS_DIR=~/Videos
+export XDG_TEMPLATES_DIR=~/Templates
+export XDG_PUBLICSHARE_DIR=~/Public
+
 # Number of commands help in memory during bash session
 HISTSIZE=5000
 # History Size
