7537eae34b29c5ea0f13a193f1d1bdd64dbf00ec
kiran94 <kiran_patel94@hotmail.com>
2020-12-31 11:09:39 +0000

    moved PATH declarations to applications sh

--- bash/application.sh
+++ bash/application.sh
@@ -1,6 +1,8 @@
 # Path
 PATH=$PATH:$HOME/.dotnet/tools/
-PATH=$PATH:$HOME/go/bin/
+export PATH=$PATH:$HOME/go/bin/
+export PATH=$PATH:/home/kiran/.local/bin
+export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin # macOS 
 
 # ASP.NET
 export ASPNETCORE_ENVIRONMENT=Development
