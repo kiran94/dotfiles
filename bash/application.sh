84701b7a545475f78740c5688196f9241290f4ea
kiran94 <kiran_patel94@hotmail.com>
2021-05-12 08:48:54 +0100

    updated applications bash

--- bash/application.sh
+++ bash/application.sh
@@ -2,9 +2,10 @@
 export PATH=$PATH:$HOME/.dotnet/tools/
 export PATH=$PATH:$HOME/go/bin/
 export PATH=$PATH:$HOME/.local/bin
-export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin # macOS 
+export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin # macOS
 
 # ASP.NET
 export ASPNETCORE_ENVIRONMENT=Development
 [ -f /mnt/c/Users/$USER/.aspnet/https/aspnetapp.pfx ]; export ASPNETCORE_Kestrel__Certificates__Default__Path=/mnt/c/Users/$USER/.aspnet/https/aspnetapp.pfx
 
+export DOTNET_CLI_TELEMETRY_OPTOUT=1
