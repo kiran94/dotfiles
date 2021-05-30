e47c437aa12d175ad618f49bff2f7f9351e8a95c
kiran94 <kiran_patel94@hotmail.com>
2021-05-30 10:49:31 +0100

    added spark shell to path

--- bash/application.sh
+++ bash/application.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:$HOME/.dotnet/tools/
 export PATH=$PATH:$HOME/go/bin/
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin # macOS
+export PATH=$PATH:/usr/local/bin/spark/bin/
 
 # ASP.NET
 export ASPNETCORE_ENVIRONMENT=Development
