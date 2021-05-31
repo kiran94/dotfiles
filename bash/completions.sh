1004855b368469f860d5c0f928b9c8fb0351d78c
kiran94 <kiran_patel94@hotmail.com>
2021-05-31 20:03:09 +0100

    exclude completion when not in bash

--- bash/completions.sh
+++ bash/completions.sh
@@ -1,2 +1,11 @@
-eval "$(gh completion -s bash)"
+#!/bin/bash
+
+[ -n "$BASH_VERSION" ] && [ -n "$DOTFILES_DEBUG" ] && `echo $BASH_SOURCE`
+[ -n "$ZSH_VERSION" ] && [ -n "$DOTFILES_DEBUG" ] && echo "$0"
+
+# Bash
+[ -n "$BASH_VERSION" ] && eval "$(gh completion -s bash)"
+
+# Zsh
+# [ -n "$ZSH_VERSION" ] && eval "$(gh completion -s zsh)"
 
