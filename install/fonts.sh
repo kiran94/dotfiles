eea7a6a0a9f827b9194757fce59e8e42d54c1008
kiran94 <kiran_patel94@hotmail.com>
2021-05-31 16:27:41 +0100

    updated tmux

---
+++ install/fonts.sh
@@ -0,0 +1,7 @@
+
+# Source Code Pro
+mkdir -p ~/.fonts
+curl --location https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/SourceCodePro/Regular/complete/Sauce%20Code%20Pro%20Nerd%20Font%20Complete.ttf 
+
+
+# Once they have been pulled
