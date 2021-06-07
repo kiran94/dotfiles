baf10eafdef19561df633d787e8af8093f080a1a
kiran94 <kiran_patel94@hotmail.com>
2021-06-07 22:34:31 +0100

    ensured tab compeltion works

--- nvim/plugins/lsp-compe.lua
+++ nvim/plugins/lsp-compe.lua
@@ -1,32 +1,32 @@
 
 vim.o.completeopt = "menuone,noselect"
-vim.o.shortmess = "c"
+vim.o.shortmess   = "c"
 
 require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'always';
-  throttle_time = 80;
-  source_timeout = 200;
+  enabled          = true;
+  autocomplete     = true;
+  debug            = false;
+  min_length       = 1;
+  preselect        = 'always';
+  throttle_time    = 80;
+  source_timeout   = 200;
   incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
+  max_abbr_width   = 100;
+  max_kind_width   = 100;
+  max_menu_width   = 100;
+  documentation    = true;
 
-  source = {
-    path = true;
-    buffer = true;
-    calc = true;
-    vsnip = true;
-    nvim_lsp = true;
-    nvim_lua = true;
-    spell = true;
-    tags = true;
+  source          = {
+    path          = true;
+    buffer        = true;
+    calc          = true;
+    vsnip         = true;
+    nvim_lsp      = true;
+    nvim_lua      = true;
+    spell         = true;
+    tags          = true;
     snippets_nvim = true;
-    treesitter = true;
+    treesitter    = true;
   };
 }
 
@@ -74,5 +74,8 @@ vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+-- vim.api.nvim_set_keymap('i', '<C-Space>', [[compe#complete()]], {expr = true})
+-- vim.api.nvim_set_keymap('i', '<CR>', [[compe#confirm('<CR>')]], {expr = true})
 
 
