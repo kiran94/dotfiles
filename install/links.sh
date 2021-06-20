e7ef6d27a2bc1a932e2ae8eec222ef73ee4c0936
kiran94 <kiran_patel94@hotmail.com>
2021-06-20 20:44:57 +0100

    updated linking script

--- install/links.sh
+++ install/links.sh
@@ -3,18 +3,18 @@
 DOTFILESSYNC_CONFIG="${DOTFILESSYNC_CONFIG:-$HOME/.config/dotfiles/dotfiles-sync.json}"
 DOTFILESSYNC_DIR="${DOTFILESSYNC_DIR:-$HOME/.config/dotfiles}"
 
-echo $DOTFILESSYNC_DIR
+# echo $DOTFILESSYNC_DIR
 
-ln -vns $DOTFILESSYNC_DIR/alacritty ~/.config/alacritty
-ln -vns $DOTFILESSYNC_DIR/nvim ~/.config/nvim
-ln -vns $DOTFILESSYNC_DIR/bash ~/.config/bash
-ln -vns $DOTFILESSYNC_DIR/bash/.bashrc ~/.bashrc
-ln -vns $DOTFILESSYNC_DIR/zsh/.zshrc ~/.zshrc
-ln -vns $DOTFILESSYNC_DIR/zsh/.p10k.zsh ~/.p10k.zsh
-ln -vns $DOTFILESSYNC_DIR/git/.gitconfig ~/.gitconfig
-ln -vns $DOTFILESSYNC_DIR/tmux/.tmux.conf ~/.tmux.conf
-ln -vns $DOTFILESSYNC_DIR/starship/starship.toml ~/.config/starship.toml
-ln -vns $DOTFILESSYNC_DIR/pgcli/config ~/.config/pgcli/config
-ln -vns ~/.config/dotfiles/neofetch/config.conf ~/.config/neofetch/config.conf
-ln -vns ~/.config/dotfiles/conda/.condarc ~/.condarc
-ln -vns ~/.config/dotfiles/ptpython/config.py ~/.config/ptpython/config.py
+ln -vnsf $DOTFILESSYNC_DIR/alacritty ~/.config/alacritty
+ln -vnsf $DOTFILESSYNC_DIR/nvim ~/.config/nvim
+ln -vnsf $DOTFILESSYNC_DIR/bash ~/.config/bash
+ln -vnsf $DOTFILESSYNC_DIR/bash/.bashrc ~/.bashrc
+ln -vnsf $DOTFILESSYNC_DIR/zsh/.zshrc ~/.zshrc
+ln -vnsf $DOTFILESSYNC_DIR/zsh/.p10k.zsh ~/.p10k.zsh
+ln -vnsf $DOTFILESSYNC_DIR/git/.gitconfig ~/.gitconfig
+ln -vnsf $DOTFILESSYNC_DIR/tmux/.tmux.conf ~/.tmux.conf
+ln -vnsf $DOTFILESSYNC_DIR/starship/starship.toml ~/.config/starship.toml
+ln -vnsf $DOTFILESSYNC_DIR/pgcli/config ~/.config/pgcli/config
+ln -vnsf $DOTFILESSYNC_DIR/neofetch/config.conf ~/.config/neofetch/config.conf
+ln -vnsf $DOTFILESSYNC_DIR/conda/.condarc ~/.condarc
+ln -vnsf $DOTFILESSYNC_DIR/ptpython/config.py ~/.config/ptpython/config.py
