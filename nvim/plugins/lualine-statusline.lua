760a391d6071c6481a9e26be24a605ab3f0fa377
kiran94 <kiran_patel94@hotmail.com>
2021-06-21 20:25:52 +0100

    added lsp active to status line

--- nvim/plugins/lualine-statusline.lua
+++ nvim/plugins/lualine-statusline.lua
@@ -1,4 +1,20 @@
 
+-- Gets the Active LSP
+local lsp_active = function()
+    local msg = '🔌'
+    local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
+    local clients = vim.lsp.get_active_clients()
+    if next(clients) == nil then return msg end
+    for _, client in ipairs(clients) do
+      local filetypes = client.config.filetypes
+      if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
+        return client.name
+      end
+    end
+    return msg
+end
+
+----------------------------------
 require'lualine'.setup {
   options = {
     icons_enabled = true,
@@ -15,7 +31,7 @@ require'lualine'.setup {
     lualine_a = {'mode'},
     lualine_b = {'branch', 'diff'},
     lualine_c = {'filename', 'diagnostics'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {'encoding', 'fileformat', 'filetype', lsp_active},
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
