675d5d24dd1ea537dbd37f66331c1cfcd5dcac0c
kiran94 <kiran_patel94@hotmail.com>
2021-07-07 07:51:12 +0100

    added file ignore patterns to telescope

--- nvim/plugins/telescope.lua
+++ nvim/plugins/telescope.lua
@@ -1,4 +1,15 @@
 require('telescope').setup{
-  prompt_position = "bottom"
+  prompt_position = "bottom",
+  defaults = {
+          file_ignore_patterns = {
+                  ".env/",
+                  ".venv/",
+                  ".git/",
+                  ".terraform/",
+                  "node_modules/",
+                  "bin/",
+                  "obj/"
+          }
+  }
 }
 
