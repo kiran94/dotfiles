25c83452c0a65c2d0b0a4c8115d27166549979e4
kiran94 <kiran_patel94@hotmail.com>
2021-10-04 07:54:12 +0100

    refactor: added dotnet nologo environment variable

--- bash/application.sh
+++ bash/application.sh
@@ -10,15 +10,12 @@ export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.yarn/bin
 export PATH=$PATH:/usr/local/bin/spark/bin/
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin # macOS
-
 export PATH=$HOME/dotnet-previews:$PATH
 
-# ASP.NET
 export ASPNETCORE_ENVIRONMENT=Development
 export ASPNETCORE_Kestrel__Certificates__Default__Path=~/.certificates/localhost.pfx
 export ASPNETCORE_Kestrel__Certificates__Default__Password=Password123
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
-
+export DOTNET_NOLOGO=1
 export PYSPARK_DRIVER_PYTHON=ptpython
-
 export GOBIN=$HOME/go/bin
