fe5314f74c0a185dffa0b0fcc338547edee25c52
kiran94 <kiran_patel94@hotmail.com>
2021-10-16 13:46:28 +0100

    refactor(go): updated paths

--- bash/application.sh
+++ bash/application.sh
@@ -5,12 +5,13 @@
 
 # Path
 export PATH=$PATH:$HOME/.dotnet/tools/
-export PATH=$PATH:$HOME/go/bin/
 export PATH=$PATH:$HOME/.local/bin
 export PATH=$PATH:$HOME/.yarn/bin
 export PATH=$PATH:/usr/local/bin/spark/bin/
 export PATH=$PATH:/Library/Frameworks/Python.framework/Versions/3.9/bin # macOS
 export PATH=$HOME/dotnet-previews:$PATH
+export GOPATH=$HOME/Documents/projects/go
+export PATH=$PATH:$GOPATH/bin
 
 export ASPNETCORE_ENVIRONMENT=Development
 export ASPNETCORE_Kestrel__Certificates__Default__Path=~/.certificates/localhost.pfx
@@ -18,4 +19,3 @@ export ASPNETCORE_Kestrel__Certificates__Default__Password=Password123
 export DOTNET_CLI_TELEMETRY_OPTOUT=1
 export DOTNET_NOLOGO=1
 export PYSPARK_DRIVER_PYTHON=ptpython
-export GOBIN=$HOME/go/bin
