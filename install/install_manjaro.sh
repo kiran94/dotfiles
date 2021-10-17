ebb8f20477d393e5dfbaa0a1635bff210af059f2
kiran94 <kiran_patel94@hotmail.com>
2021-10-17 20:37:49 +0100

    feat(manjaro): added docker-compose, ptpython and terraform

--- install/install_manjaro.sh
+++ install/install_manjaro.sh
@@ -11,12 +11,20 @@ sudo pacman -S github-cli
 sudo pacman -S python-pip
 sudo pacman -S redis
 sudo pacman -S mpv
+sudo pacman -S net-tools
+sudo pacman -S terraform
 
 sudo yay -S gnome-shell-extension-pop-shell-git
 sudo yay -S brave-browser
 sudo yay -S expressvpn
 sudo yay -S neofetch
 sudo yay -S pgcli
+sudo yay -S ani-cli-git
+sudo yay -S ptpython
+
+# https://wiki.manjaro.org/index.php/Power_Management
+sudo yay -S tlp
+sudo yay -S tlpui
 
 # Vim Plugins
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \\n       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'\n
@@ -34,7 +42,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Docker
-sudo pacman -S docker
+sudo pacman -S docker docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
 usermod --append --groups docker kiran # requires log in/out
@@ -57,7 +65,6 @@ sudo -iu postgres initdb -D /var/lib/postgres/data/
 sudo -iu postgres createuser --createdb --login --createrole --superuser kiran # or createuser --interactive
 sudo systemctl start postgresql
 sudo -u postgres psql -c "ALTER USER kiran PASSWORD 'your-password';"
-
 # Give root admin as password on both db and os
 sudo passwd postgres
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'your-password';"
