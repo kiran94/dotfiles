e77628b0e25ef014816438547d489ed418bab09f
kiran94 <kiran_patel94@hotmail.com>
2021-12-05 15:10:52 +0000

    refactor: added lastspace ignores

--- nvim/lua/plugins/misc/nvim-lastplace.lua
+++ nvim/lua/plugins/misc/nvim-lastplace.lua
@@ -1,7 +1,10 @@
 local options = {}
 
 options.config = function()
-    require "nvim-lastplace".setup {}
+    require "nvim-lastplace".setup {
+        lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
+        lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"}
+    }
 end
 
 return options
