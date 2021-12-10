1ba34af1f866da88238f143995de305e614abde6
kiran94 <kiran_patel94@hotmail.com>
2021-12-10 08:37:53 +0000

    refactor(nvim): added built in plugins

--- nvim/lua/myplugins.lua
+++ nvim/lua/myplugins.lua
@@ -70,3 +70,15 @@ augroup packer_user_config
   autocmd BufWritePost myplugins.lua source <afile> | PackerCompile
 augroup end
 ]]
+
+-- Built In Plugins
+vim.g.loaded_gzip         = 1
+vim.g.loaded_tar          = 1
+vim.g.loaded_tarPlugin    = 1
+vim.g.loaded_zipPlugin    = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_netrw        = 1
+vim.g.loaded_netrwPlugin  = 1
+vim.g.loaded_matchit      = 1
+vim.g.loaded_matchparen   = 1
+vim.g.loaded_spec         = 1
