2b924852ee6bea73fe60d4e60d4e6f10c61f8911
kiran94 <kiran_patel94@hotmail.com>
2021-12-19 14:23:53 +0000

    refactor(nvim): tweak lazy loaded plugins

--- nvim/lua/myplugins.lua
+++ nvim/lua/myplugins.lua
@@ -5,37 +5,36 @@ packer.startup(function()
 
     use { 'goolord/alpha-nvim',                  config = require'plugins/misc/dashboard'.config, requires = { 'kyazdani42/nvim-web-devicons' } }
     use { 'lewis6991/gitsigns.nvim',             config = require'plugins/editor/gitsigns'.config, requires = { 'nvim-lua/plenary.nvim' } }
-    use { 'folke/which-key.nvim',                config = require'plugins/misc/which-key'.config }
+    use { 'folke/which-key.nvim',                config = require'plugins/misc/which-key'.config, keys = '<leader>' }
     use { 'windwp/nvim-autopairs',               config = require'plugins/editor/nvim-autopairs'.config }
-    use { 'ethanholz/nvim-lastplace',            config = require'plugins/misc/nvim-lastplace'.config}
+    use { 'ethanholz/nvim-lastplace',            config = require'plugins/misc/nvim-lastplace'.config }
     use { 'ahmedkhalf/project.nvim',             config = require'plugins/misc/project'.config }
     use { 'folke/todo-comments.nvim',            config = require'plugins/editor/todo-comments'.config, requires = {"nvim-lua/plenary.nvim" }}
     use { 'numToStr/Comment.nvim',               config = require'plugins/editor/comment'.config }
     use { 'chentau/marks.nvim',                  config = require'plugins/editor/marks'.config }
     use { 'norcalli/nvim-colorizer.lua',         config = require'plugins/editor/colorizer'.config, cmd = {'ColorizerToggle'} }
-    use { 'kyazdani42/nvim-tree.lua',            config = require'plugins/editor/nvim-tree'.config, requires = { 'kyazdani42/nvim-web-devicons'} }
+    use { 'kyazdani42/nvim-tree.lua',            config = require'plugins/editor/nvim-tree'.config, requires = { 'kyazdani42/nvim-web-devicons'}, cmd = {"NvimTreeToggle"} }
     use { 'nvim-telescope/telescope.nvim',       config = require'plugins/misc/telescope'.config, requires = { {'nvim-lua/plenary.nvim'} } }
     use { 'hoob3rt/lualine.nvim',                config = require'plugins/misc/lualine'.config, requires = {'kyazdani42/nvim-web-devicons', opt = true} }
     use { 'akinsho/nvim-bufferline.lua',         config = require'plugins/misc/bufferline'.config }
     -- use { 'mhartington/formatter.nvim',       config = require'plugins/editor/formatter'.config }
-    use { 'danymat/neogen',                      config = require'plugins/editor/neogen'.config, requires = "nvim-treesitter/nvim-treesitter" }
+    use { 'danymat/neogen',                      config = require'plugins/editor/neogen'.config, requires = "nvim-treesitter/nvim-treesitter", cmd = {"Neogen"} }
     use { 'lukas-reineke/indent-blankline.nvim', config = require'plugins/editor/indent-blankline'.config }
     use { 'mzlogin/vim-markdown-toc',            config = require'plugins/misc/markdown-toc'.config, cmd = { "GenTocGFM"} }
-    use { "rcarriga/vim-ultest",                 config = require'plugins/editor//ultest'.config, requires = {"vim-test/vim-test"}, run = ":UpdateRemotePlugins" }
-    use { "beauwilliams/focus.nvim",             config = require'plugins/editor/focus'.config }
+    use { 'rcarriga/vim-ultest',                 config = require'plugins/editor/ultest'.config, requires = {"vim-test/vim-test"}, run = ":UpdateRemotePlugins" }
+    use { 'beauwilliams/focus.nvim',             config = require'plugins/editor/focus'.config }
     use { 'nvim-treesitter/nvim-treesitter',     run = ':TSUpdate' }
-    use { 'nvim-treesitter/playground',          requires = {'nvim-treesitter/nvim-treesitter'}, disabled = false, cmd = {"TSPlaygroundToggle"} }
+    use { 'nvim-treesitter/playground',          requires = {'nvim-treesitter/nvim-treesitter'}, cmd = {"TSPlaygroundToggle"} }
+    use { 'jbyuki/venn.nvim',                    cmd = {'VBox', 'VBoxD', 'VBoxDO', 'VBoxH', 'VBoxHO', 'VBoxO'} }
+    use { 'ellisonleao/glow.nvim',               cmd = {'Glow', 'GlowInstall'} }
+    use { 'folke/zen-mode.nvim',                 cmd = { "ZenMode" } }
+    use { 'junegunn/vim-easy-align',             cmd = { "EasyAlign" } }
+    use { 'tweekmonster/startuptime.vim',        cmd = {"StartupTime"} }
+    use { 'gyim/vim-boxdraw',                    ft = 'markdown'}
     use { 'ChristianChiarulli/vim-solidity',     ft = {'solidity', 'sol'}}
-    use {'jbyuki/venn.nvim',                     cmd = {'VBox', 'VBoxD', 'VBoxDO', 'VBoxH', 'VBoxHO', 'VBoxO'} }
-    use {"ellisonleao/glow.nvim",                cmd = {'Glow', 'GlowInstall'} }
-    use { 'folke/zen-mode.nvim' }
-
     use 'ntpeters/vim-better-whitespace'
     use 'inside/vim-search-pulse'
-    use {'junegunn/vim-easy-align', cmd = { "EasyAlign" } }
     -- use 'gpanders/editorconfig.nvim'
-    use 'tweekmonster/startuptime.vim'
-    use 'gyim/vim-boxdraw'
 
     -- LSP
     use 'neovim/nvim-lspconfig'
@@ -44,7 +43,7 @@ packer.startup(function()
     use 'williamboman/nvim-lsp-installer'
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp'
-    use 'hrsh7th/cmp-nvim-lua'
+    use { 'hrsh7th/cmp-nvim-lua', ft = 'lua' }
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
@@ -52,13 +51,13 @@ packer.startup(function()
     use 'hrsh7th/vim-vsnip'
     use 'hrsh7th/cmp-calc'
     use 'hrsh7th/cmp-emoji'
-    use 'davidsierradz/cmp-conventionalcommits'
+    use { 'davidsierradz/cmp-conventionalcommits', ft = 'gitcommit' }
     use 'rafamadriz/friendly-snippets'
     use 'folke/lsp-colors.nvim'
     use 'b0o/SchemaStore.nvim'
     -- use { 'weilbith/nvim-code-action-menu', config = require'plugins/lsp/code-action'.config, cmd="CodeActionMenu" }
     use { 'jose-elias-alvarez/null-ls.nvim',   config = require'plugins/lsp/null-ls'.config, requires = 'nvim-lua/plenary.nvim'}
-    use { 'tami5/lspsaga.nvim',                config = require'plugins/lsp/lspsaga'.config, requires = 'neovim/nvim-lspconfig'}
+    use { 'tami5/lspsaga.nvim',                config = require'plugins/lsp/lspsaga'.config, requires = 'neovim/nvim-lspconfig' }
 
     -- WARN: Needs with Neovim 0.6 dependency
     -- use { "folke/trouble.nvim", config = require'plugins/lsp/trouble'.config, requires = "kyazdani42/nvim-web-devicons" }
