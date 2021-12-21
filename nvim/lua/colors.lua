491aa798effc6fee9aaf41f4ec7cf991865de52e
kiran94 <kiran_patel94@hotmail.com>
2021-12-21 03:54:34 +0000

    refactor(nvim): update colorscheme handling to be safe

--- nvim/lua/colors.lua
+++ nvim/lua/colors.lua
@@ -1,12 +1,13 @@
-vim.cmd [[colorscheme codedark]]
+local colorscheme = "codedark"
 
--- vim.cmd [[colorscheme rose-pine]]
--- vim.cmd [[colorscheme lighthaus]]
--- vim.cmd [[colorscheme github_dark_default]]
--- vim.cmd [[colorscheme tokyonight]]
+-- Safely attempt to set the colorscheme
+-- if not installed then just notify
+local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not ok then
+    vim.notify("colorscheme " .. colorscheme .. " not found")
+end
 
 vim.cmd [[highlight TelescopeMatching guifg=#8b0000]]
 vim.cmd [[highlight Search guibg=Yellow guifg=Black gui=NONE]]
 vim.cmd [[highlight Visual guifg=Black guibg=Orange gui=none]]
-
 vim.cmd [[highlight LspSagaCodeActionContent guifg=White gui=none]]
