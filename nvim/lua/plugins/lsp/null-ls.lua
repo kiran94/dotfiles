2d034dc5aa3887234b49385fd09d22bc9ee0fed3
kiran94 <kiran_patel94@hotmail.com>
2021-12-24 21:21:16 +0000

    style(nvim): add spacing in null-ls

--- nvim/lua/plugins/lsp/null-ls.lua
+++ nvim/lua/plugins/lsp/null-ls.lua
@@ -8,16 +8,20 @@ options.config = function ()
             -- Python
             null_ls.builtins.formatting.autopep8,
             null_ls.builtins.formatting.isort,
+
             -- Go
             null_ls.builtins.formatting.gofmt,
             null_ls.builtins.formatting.goimports,
             -- null_ls.builtins.formatting.golines,
+
             -- Lua
             null_ls.builtins.formatting.lua_format,
             -- null_ls.builtins.formatting.stylua,
+
             -- Others
             null_ls.builtins.formatting.terraform_fmt,
             null_ls.builtins.formatting.clang_format,
+
             -- JavaScript
             null_ls.builtins.diagnostics.eslint,
             -- null_ls.builtins.diagnostics.prettier,
