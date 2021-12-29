6220551936faab51c386a2a4143d72451f7fdd9e
kiran94 <kiran_patel94@hotmail.com>
2021-12-29 06:32:33 +0000

    refactor(nvim): enable stylua formatting

--- nvim/lua/plugins/lsp/null-ls.lua
+++ nvim/lua/plugins/lsp/null-ls.lua
@@ -1,33 +1,32 @@
 local options = {}
 
-options.config = function ()
+options.config = function()
+	local null_ls = require("null-ls")
+	null_ls.setup({
+		sources = {
+			-- Python
+			null_ls.builtins.formatting.autopep8,
+			null_ls.builtins.formatting.isort,
 
-    local null_ls = require('null-ls')
-    null_ls.setup({
-        sources = {
-            -- Python
-            null_ls.builtins.formatting.autopep8,
-            null_ls.builtins.formatting.isort,
+			-- Go
+			null_ls.builtins.formatting.gofmt,
+			null_ls.builtins.formatting.goimports,
+			-- null_ls.builtins.formatting.golines,
 
-            -- Go
-            null_ls.builtins.formatting.gofmt,
-            null_ls.builtins.formatting.goimports,
-            -- null_ls.builtins.formatting.golines,
+			-- Lua
+			-- null_ls.builtins.formatting.lua_format,
+			null_ls.builtins.formatting.stylua,
 
-            -- Lua
-            -- null_ls.builtins.formatting.lua_format,
-            null_ls.builtins.formatting.stylua,
+			-- Others
+			null_ls.builtins.formatting.terraform_fmt,
+			null_ls.builtins.formatting.clang_format,
 
-            -- Others
-            null_ls.builtins.formatting.terraform_fmt,
-            null_ls.builtins.formatting.clang_format,
-
-            -- JavaScript
-            null_ls.builtins.diagnostics.eslint,
-            -- null_ls.builtins.diagnostics.prettier,
-            null_ls.builtins.formatting.json_tool,
-        },
-    })
+			-- JavaScript
+			-- null_ls.builtins.diagnostics.eslint,
+			-- null_ls.builtins.diagnostics.prettier,
+			null_ls.builtins.formatting.json_tool,
+		},
+	})
 end
 
 return options
