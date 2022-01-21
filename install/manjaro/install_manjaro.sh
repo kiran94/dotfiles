6b287ed45ee854e6dd37d1d4c7ed2f13550bae75
kiran94 <kiran_patel94@hotmail.com>
2022-01-21 09:27:43 +0000

    refactor(manjaro): move install manjaro script

--- install/manjaro/install_manjaro.sh
+++ install/manjaro/install_manjaro.sh
@@ -1,55 +1,8 @@
 #!/bin/bash
 
-sudo pacman -Syu
-sudo pacman -S vim tmux git
-sudo pacman -S neovim
-sudo pacman -S tree
-sudo pacman -S htop
-sudo pacman -S fzf ripgrep jq
-sudo pacman -S npm yarn
-sudo pacman -S alacritty
-sudo pacman -S github-cli
-sudo pacman -S python-pip
-sudo pacman -S python-pipenv
-sudo pacman -S redis
-sudo pacman -S mpv
-sudo pacman -S net-tools
-sudo pacman -S terraform
-sudo pacman -S starship
-sudo pacman -S bluez-utils
-sudo pacman -S ranger
-sudo pacman -S speedtest-cli
-
-yay -Syu
-yay -S gnome-shell-extension-pop-shell-git
-yay -S brave-browser
-yay -S expressvpn
-yay -S neofetch
-yay -S pgcli
-yay -S ani-cli-git
-yay -S ptpython
-
-# https://wiki.manjaro.org/index.php/Power_Management
-sudo yay -S tlp
-sudo yay -S tlpui
-# OR
-sudo yay -S laptop-mode-tools
-sudo systemctl enable --now laptop-mode.service
-
-# Vim Plugins
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \\n       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'\n
-
-# Tmux Plugins
-mkdir -p ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-# Yarn Installs
-yarn global add tldr
-yarn global add gitignore.cli
-
 # Zsh Plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 # Docker
 sudo pacman -S docker docker-compose
@@ -62,7 +15,7 @@ systemctl enable redis
 systemctl status redis
 
 # Sound
-# generatel sound worked out the box
+# generally sound worked out the box
 # but this step was required to get it
 # working with mpv player
 # https://forums.debian.net//viewtopic.php?t=123902
@@ -79,17 +32,11 @@ sudo -u postgres psql -c "ALTER USER kiran PASSWORD 'your-password';"
 sudo passwd postgres
 sudo -u postgres psql -c "ALTER USER postgres PASSWORD 'your-password';"
 
-# Remove Gnome Packages
-sudo pacman -R lollypop
-sudo pacman -R geary
-sudo pacman -R gnome-maps
-
 # Visidata
 yay -S visidata
 # for parquet support
 # these need to be installed globally
-python -m pip install pandas
-python -m pip install pyarrow
+python -m pip install pandas pyarrow
 
 # Apache Spark
 sudo pacman -S jre11-openjdk-headless jdk11-openjdk
