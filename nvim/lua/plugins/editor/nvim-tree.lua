b06c9b1db429ac54f8ceb67cb23556ed0e59bc50
kiran94 <kiran_patel94@hotmail.com>
2022-04-28 19:14:05 +0100

    refactor(nvim-tree): updated configuration

--- nvim/lua/plugins/editor/nvim-tree.lua
+++ nvim/lua/plugins/editor/nvim-tree.lua
@@ -3,44 +3,44 @@ local options = {}
 options.config = function()
 
     require'nvim-tree'.setup {
-      disable_netrw       = true,  -- disables netrw completely
-      hijack_netrw        = true,  -- hijack netrw window on startup
-      open_on_setup       = false, -- open the tree when running this setup function
-      ignore_ft_on_setup  = {},    -- will not open on setup if the filetype is in this list
-      open_on_tab         = true,  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
-
+      disable_netrw      = true,  -- disables netrw completely
+      hijack_netrw       = true,  -- hijack netrw window on startup
+      open_on_setup      = false, -- open the tree when running this setup function
+      open_on_tab        = true,  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+      hijack_cursor      = true,  -- hijack the cursor in the tree to put it at the start of the filename
+      update_cwd         = true,  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+      ignore_ft_on_setup = {},    -- will not open on setup if the filetype is in this list
+      renderer = {
+        indent_markers = {
+          enable = true,
+          icons = {
+            corner = "└ ",
+            edge = "│ ",
+            none = "  ",
+          },
+        },
+        icons = {
+          webdev_colors = true,
+        },
+      },
       -- hijacks new directory buffers when they are opened.
-      update_to_buf_dir   = {
+      hijack_directories   = {
         enable    = true,  -- enable the feature
         auto_open = false, -- allow to open the tree if it was previously closed
       },
-
-      hijack_cursor = true, -- hijack the cursor in the tree to put it at the start of the filename
-      update_cwd    = true, -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-
       -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
       update_focused_file = {
         enable      = true,
         update_cwd  = true,
         ignore_list = { '.git', 'node_modules', 'cache', '.mypy_cache'  }
       },
-
-      -- configuration options for the system open command (`s` in the tree by default)
-      system_open = {
-        cmd  = nil, -- the command to run this, leaving nil should work in most cases
-        args = {} -- the command arguments as a list
-      },
-
       filters = {
-          -- hide dotfiles
-          dotfiles = false
+        dotfiles = false -- hide dotfiles
       },
-
       view = {
         width       = 40,        -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
-        -- height      = 30,     -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+        height      = 30,     -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
         side        = 'right',   -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
-        auto_resize = false,     -- if true the tree will resize itself after opening a file
         mappings = {
           -- custom only false will merge the list with the default mappings
           -- if true, it will only use your list to set the mappings
@@ -48,7 +48,12 @@ options.config = function()
           -- list of mappings to set on the tree manually
           list = {}
         }
-      }
+      },
+      actions = {
+            open_file = {
+                resize_window = true
+            }
+        },
     }
 
     -- Automatically close nvim when nvim-tree is the last window
