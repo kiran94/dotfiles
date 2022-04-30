4bf7c895b3d6ba614d8a06213c0d670c45f59bff
kiran94 <kiran_patel94@hotmail.com>
2022-04-30 21:50:34 +0100

    style(lsp): cleaned up text document handlers

--- nvim/lua/plugins/lsp/lsp-installer.lua
+++ nvim/lua/plugins/lsp/lsp-installer.lua
@@ -27,13 +27,8 @@ vim.diagnostic.config({
 	update_in_insert = true,
 })
 
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "rounded"
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    border = "rounded"
-})
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = false;
