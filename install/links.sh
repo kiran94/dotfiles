993422ce0b71ceabc1ead9cbea93e0206ae7023d
kiran94 <kiran_patel94@hotmail.com>
2022-05-14 14:28:54 +0100

    feat: add x11

--- install/links.sh
+++ install/links.sh
@@ -26,3 +26,12 @@ ln -vnsf $DOTFILESSYNC_DIR/vim/.vimrc ~/.vimrc
 ln -vnsf $DOTFILESSYNC_DIR/bspwm/bspwmrc ~/.config/bspwm/bspwmrc
 ln -vnsf $DOTFILESSYNC_DIR/sxhkd/sxhkdrc ~/.config/sxhkd/sxhkdrc
 ln -vnsf $DOTFILESSYNC_DIR/picom/picom.conf ~/.config/picom/picom.conf
+ln -vs $DOTFILESSYNC_DIR/polybar ~/.config/polybar
+
+ln -vs $DOTFILESSYNC_DIR/x11/.Xresources ~/.Xresources
+
+
+
+
+
+git clone git@github.com:kiran94/wallpapers.git ~/.config/wallpapers
