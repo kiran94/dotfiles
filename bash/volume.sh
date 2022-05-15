4e514921415fac5036c1ab2a1bea6521c7f26cb2
kiran94 <kiran_patel94@hotmail.com>
2022-05-15 21:42:36 +0100

    refactor: updated volume commands

--- bash/volume.sh
+++ bash/volume.sh
@@ -3,12 +3,10 @@
 [ -n "$BASH_VERSION" ] && [ -n "$DOTFILES_DEBUG" ] && `echo $BASH_SOURCE`
 [ -n "$ZSH_VERSION" ] && [ -n "$DOTFILES_DEBUG" ] && echo "$0"
 
-# https://unix.stackexchange.com/a/21090
-alias volume0="amixer set Master 0%"
-alias volume25="amixer set Master 25%"
-alias volume50="amixer set Master 50%"
-alias volume75="amixer set Master 75%"
-alias volume100="amixer set Master 100%"
+
+# https://wiki.archlinux.org/title/Xbindkeys#Backlight_control
+alias volume_increase="pactl set-sink-volume @DEFAULT_SINK@ +1000"
+alias volume_increase="pactl set-sink-volume @DEFAULT_SINK@ -1000"
 
 alias brightness_increase="xbacklight -inc 10"
 alias brightness_decrease="xbacklight -dec 10"
