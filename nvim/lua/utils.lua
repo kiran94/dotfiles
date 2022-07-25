b44963f295a05ee2299a9eff936d40c1387d3164
kiran94 <kiran_patel94@hotmail.com>
2022-07-25 04:09:43 +0100

    feat: add packer sync snapshot

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -10,3 +10,19 @@ function _G.put(...)
 	print(table.concat(objects, "\n"))
 	return ...
 end
+
+local async = require("plenary.async")
+
+local packer_sync = function()
+	async.run(function()
+		vim.notify.async("Syncing packer.", "info", {
+			title = "Packer",
+		})
+	end)
+	local snap_shot_time = os.date("!%Y-%m-%dT%TZ")
+	vim.cmd("PackerSnapshot " .. snap_shot_time)
+	vim.cmd("PackerSync")
+end
+
+vim.keymap.set("n", "<leader>ps", function() packer_sync() end)
+
