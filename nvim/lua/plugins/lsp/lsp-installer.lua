44e3be3e7859e102e857eb62dd159f5e3412f7a6
kiran94 <kiran_patel94@hotmail.com>
2022-09-25 10:32:02 +0100

    refactor(lsp): disable virtual text

--- nvim/lua/plugins/lsp/lsp-installer.lua
+++ nvim/lua/plugins/lsp/lsp-installer.lua
@@ -30,6 +30,7 @@ vim.diagnostic.config({
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with( vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = false })
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = false;
