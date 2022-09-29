a64fe3fa42f9b6c1e9112f70c2dbd0ddb785b08a
kiran94 <kiran_patel94@hotmail.com>
2022-09-29 20:36:51 +0100

    refactor(nvim): border cmp windows

--- nvim/lua/plugins/lsp/lsp-installer.lua
+++ nvim/lua/plugins/lsp/lsp-installer.lua
@@ -231,6 +231,10 @@ options.config = function()
                 vim.fn["vsnip#anonymous"](args.body)
             end
         },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
         mapping = {
           ['<C-n>']     = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
           ['<C-p>']     = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
