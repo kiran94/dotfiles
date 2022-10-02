4f2d10f70c877baef359234c24267543e3461da5
kiran94 <kiran_patel94@hotmail.com>
2022-10-02 16:41:30 +0100

    refactor(nvim): updated notify background color

--- nvim/lua/plugins/misc/notify.lua
+++ nvim/lua/plugins/misc/notify.lua
@@ -7,13 +7,13 @@ options.config = function()
 	vim.notify = notify
 
 	notify.setup({
-		stages = "fade",              -- Animation style (see below for details)
-		on_open = nil,                -- Function called when a new window is opened, use for changing win settings/config
-		on_close = nil,               -- Function called when a window is closed
-		render = "default",           -- Render function for notifications. See notify-render()
-		timeout = 3000,               -- Default timeout for notifications
-		background_colour = "Normal", -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
-		minimum_width = 50,           -- Minimum width for notification windows
+		stages = "fade",               -- Animation style (see below for details)
+		on_open = nil,                 -- Function called when a new window is opened, use for changing win settings/config
+		on_close = nil,                -- Function called when a window is closed
+		render = "default",            -- Render function for notifications. See notify-render()
+		timeout = 3000,                -- Default timeout for notifications
+		background_colour = "#000000", -- Set this to either a highlight group or an RGB hex value e.g. "#000000"
+		minimum_width = 50,            -- Minimum width for notification windows
 
 		icons = {
 			ERROR = "",
