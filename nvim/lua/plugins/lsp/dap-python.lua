6ffbe0b535d1226898e69b4400c500edfde66679
kiran94 <kiran_patel94@hotmail.com>
2022-10-10 20:03:57 +0100

    style(dap): cleanup python adapter

--- nvim/lua/plugins/lsp/dap-python.lua
+++ nvim/lua/plugins/lsp/dap-python.lua
@@ -8,7 +8,10 @@ M.setup = function(dap, install_directory)
 		dap_python.setup(p)
 		dap_python.test_runner = "pytest"
 	else
-		dap.adapters.python = { type = "executable", command = install_directory .. "debugpy-adapter" }
+		dap.adapters.python = {
+			type = "executable",
+			command = install_directory .. "debugpy-adapter",
+		}
 
 		dap.configurations.python = {}
 		table.insert(dap.configurations.python, {
