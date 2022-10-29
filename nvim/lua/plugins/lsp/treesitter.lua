19d9bce256aac12547a2f28bc8f69cab987953f9
kiran94 <kiran_patel94@hotmail.com>
2022-10-29 12:04:31 +0100

    refactor(nvim): add iswap config

--- nvim/lua/plugins/lsp/treesitter.lua
+++ nvim/lua/plugins/lsp/treesitter.lua
@@ -2,6 +2,7 @@ local options = {}
 
 options.config = function()
 	local treesitter = require("nvim-treesitter.configs")
+	local iswapOk, iswap = pcall(require, "iswap")
 
 	treesitter.setup({
 		ensure_installed = "all",
@@ -14,6 +15,13 @@ options.config = function()
 			enable = true,
 		},
 	})
+
+	if iswapOk then
+		iswap.setup({
+			-- Automatically swap with only two arguments
+			autoswap = true,
+		})
+	end
 end
 
 return options
