425faddbe586f58c170479ad81940ba20f3e5f07
kiran94 <kiran_patel94@hotmail.com>
2022-10-30 10:14:11 +0000

    style(nvim): format lualine

--- nvim/lua/plugins/misc/lualine.lua
+++ nvim/lua/plugins/misc/lualine.lua
@@ -1,97 +1,102 @@
 local options = {}
 
 options.config = function()
-    -- Gets the Active LSP
-    local lsp_active = function()
-        local msg = "🔌"
-        local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
+	-- Gets the Active LSP
+	local lsp_active = function()
+		local msg = "🔌"
+		local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
+		local clients = vim.lsp.get_active_clients()
 
-        -- If there are no clients then just return
-        if next(clients) == nil then
-            return msg
-        end
+		-- If there are no clients then just return
+		if next(clients) == nil then
+			return msg
+		end
 
-        -- Find all the clients attached to Buffer
-        local found_clients = {}
-        local found_client_count = 0
+		-- Find all the clients attached to Buffer
+		local found_clients = {}
+		local found_client_count = 0
 
-        for _, client in ipairs(clients) do
+		for _, client in ipairs(clients) do
+			local filetypes = client.config.filetypes
+			if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= "null-ls" then
+				table.insert(found_clients, client.name)
+				found_client_count = found_client_count + 1
+			end
+		end
 
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client.name ~= "null-ls" then
-                table.insert(found_clients, client.name)
-                found_client_count = found_client_count + 1
-            end
-        end
+		-- For any found then concat them together and return
+		if found_client_count > 0 then
+			return table.concat(found_clients, " | ")
+		end
 
-        -- For any found then concat them together and return
-        if found_client_count > 0 then
-            return table.concat(found_clients, " | ")
-        end
+		return msg
+	end
 
-        return msg
-    end
+	-- Get Python Environment
+	local python_env = function()
+		if vim.bo.filetype ~= "python" then
+			return ""
+		end
 
 		local virtual_env = os.getenv("VIRTUAL_ENV")
 		local conda_env = os.getenv("CONDA_DEFAULT_ENV")
 
 		-- Normalize the path
-        -- e.g /home/user/projects/.venv => ~/projects/.env
+		-- e.g /home/user/projects/.venv => ~/projects/.env
 		local user = os.getenv("USER")
 		local home_path = "/home/" .. user
-        virtual_env = virtual_env:gsub(home_path, "~")
+		virtual_env = virtual_env:gsub(home_path, "~")
 
-        if virtual_env ~= nil then
-            return virtual_env
-        elseif conda_env ~= nil and conda_env ~= "base" then
-            return conda_env
-        end
+		if virtual_env ~= nil then
+			return virtual_env
+		elseif conda_env ~= nil and conda_env ~= "base" then
+			return conda_env
+		end
 
-        return ""
-    end
+		return ""
+	end
 
-    local dap_status = function ()
-        local ok, dap = pcall(require, "dap")
-        if not ok then
-            return
-        end
+	local dap_status = function()
+		local ok, dap = pcall(require, "dap")
+		if not ok then
+			return
+		end
 
-        return dap.status()
-    end
+		return dap.status()
+	end
 
-    require "lualine".setup {
-        options = {
-            icons_enabled        = true,
-            globalstatus         = true,
-            theme                = "vscode", -- material
-            component_separators = {"", ""},
-            section_separators   = {"", ""},
-            disabled_filetypes   = {},
-            sources              = {"nvim_diagnostic"},
-            color_error          = "#ec5f67",
-            color_warn           = "#FF8800",
-            color_info           = "#008080",
-        },
-        sections = {
-            lualine_a = {"mode"},
-            lualine_b = {"branch", "diff"},
-            lualine_c = {"filename", "diagnostics", dap_status},
-            lualine_x = {python_env, "encoding", "fileformat", "filetype", lsp_active},
-            lualine_y = {"progress"},
-            lualine_z = {"location"}
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = {"filename"},
-            lualine_x = {"location"},
-            lualine_y = {},
-            lualine_z = {}
-        },
-        tabline = {},
-        extensions = {}
-    }
+	require("lualine").setup({
+		options = {
+			icons_enabled = true,
+			globalstatus = true,
+			theme = "vscode", -- material
+			component_separators = { "", "" },
+			section_separators = { "", "" },
+			disabled_filetypes = {},
+			sources = { "nvim_diagnostic" },
+			color_error = "#ec5f67",
+			color_warn = "#FF8800",
+			color_info = "#008080",
+		},
+		sections = {
+			lualine_a = { "mode" },
+			lualine_b = { "branch", "diff" },
+			lualine_c = { "filename", "diagnostics", dap_status },
+			lualine_x = { python_env, "encoding", "fileformat", "filetype", lsp_active },
+			lualine_y = { "progress" },
+			lualine_z = { "location" },
+		},
+		inactive_sections = {
+			lualine_a = {},
+			lualine_b = {},
+			lualine_c = { "filename" },
+			lualine_x = { "location" },
+			lualine_y = {},
+			lualine_z = {},
+		},
+		tabline = {},
+		extensions = {},
+	})
 end
 
 return options
