d636269ddca4667b839240135c9e466fcb9b8185
kiran94 <kiran_patel94@hotmail.com>
2023-11-23 20:34:43 +0000

    refactor(nvim): upgrade packages and update indent_blankline

--- nvim/lua/plugins/editor/indent-blankline.lua
+++ nvim/lua/plugins/editor/indent-blankline.lua
@@ -5,13 +5,11 @@ vim.opt.listchars:append("space:.")
 vim.opt.listchars:append("eol:↴")
 
 options.config = function()
-	require("indent_blankline").setup({
-		show_end_of_line = true,
-		space_char_blankline = " ",
-		show_current_context_start = false,
-		show_current_context = false,
-		buftype_exclude = { "terminal", "nofile" },
-		filetype_exclude = { "alpha", "NvimTree", "packer", "help" },
+	require("ibl").setup({
+		enabled = true,
+		scope = {
+			enabled = true,
+		},
 	})
 end
 
