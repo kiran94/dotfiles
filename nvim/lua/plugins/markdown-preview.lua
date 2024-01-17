3ad1956f12a434913f6e8b73bd76348a774fa6b0
kiran94 <kiran_patel94@hotmail.com>
2024-01-17 09:02:08 +0000

    fix(nvim): updated markdown preview

--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,26 +1,8 @@
 return {
 	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
 	build = function()
 		vim.fn["mkdp#util#install"]()
-	end,
-	ft = { "markdown" },
-	config = function()
-		local os = require("os")
-
-		---Gets the User Defined Browser from the Environment
-		-- Or Defaults to brave
-		local get_browser = function()
-			local user_browser = os.getenv("BROWSER")
-			local default_browser = "brave"
-
-			if user_browser == nil or user_browser == "" then
-				return default_browser
-			else
-				return user_browser
-			end
-		end
-
-		local browser = get_browser()
-		vim.cmd([[let g:mkdp_browser = browser]])
 	end,
 }
