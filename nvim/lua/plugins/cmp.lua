3235d99ba9528cf03600319efc342eb628d97ee6
kiran94 <kiran_patel94@hotmail.com>
2024-01-19 07:43:24 +0000

    refactor(nvim): ensure that nvim-lua completion only is enabled for lua files

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -2,7 +2,7 @@ return {
 	"hrsh7th/cmp-nvim-lsp",
 	dependencies = {
 		"hrsh7th/nvim-cmp",
-		"hrsh7th/cmp-nvim-lua", -- ft = lua
+		{ "hrsh7th/cmp-nvim-lua", ft = "lua" },
 		"hrsh7th/cmp-buffer",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-cmdline",
