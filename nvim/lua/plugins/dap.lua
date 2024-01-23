8bec460a21d858250bed42505333db51a3d5f5f8
kiran94 <kiran_patel94@hotmail.com>
2024-01-23 18:17:14 +0000

    refactor(nvim): updated dap configuration to be lazy

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -6,7 +6,32 @@ return {
 		"theHamsta/nvim-dap-virtual-text",
 		"mfussenegger/nvim-dap-python",
 	},
-	config = function()
-		require("plugins/lsp/dap")
+    opts = {},
+	config = function(plugin, opts)
+		--------------------------------
+		-- ADAPTERS and CONFIGURATION
+		--------------------------------
+		local mason_install_directory = vim.fn.stdpath("data") .. "/mason/" .. "bin/"
+
+		require("plugins.lsp.dap-python").setup(plugin, mason_install_directory)
+		require("plugins.lsp.dap-bash").setup(plugin, mason_install_directory)
+		require("plugins.lsp.dap-go").setup()
+
+		require("dap.ext.vscode").load_launchjs(nil, nil)
+
+		--------------------------------
+		-- SIGNS
+		--------------------------------
+		vim.fn.sign_define("DapBreakpoint", { text = "🛑", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+		vim.fn.sign_define(
+			"DapBreakpointCondition",
+			{ text = "💊", texthl = "DiagnosticSignError", linehl = "", numhl = "" }
+		)
+		vim.fn.sign_define("DapLogPoint", { text = "📝", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
+		vim.fn.sign_define(
+			"DapBreakpointRejected",
+			{ text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" }
+		)
+		vim.fn.sign_define("DapStopped", { text = "→", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 	end,
 }
