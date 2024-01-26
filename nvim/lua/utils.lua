dd06280bf3ed516cf88eb8879a29b47f322ea73a
kiran94 <kiran_patel94@hotmail.com>
2024-01-26 20:41:31 +0000

    refactor(nvim): remove packer sync

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -10,18 +10,3 @@ function _G.put(...)
 	print(table.concat(objects, "\n"))
 	return ...
 end
-
-
-local packer_sync = function()
-    local async = require("plenary.async")
-	async.run(function()
-		vim.notify.async("Syncing packer.", "info", {
-			title = "Packer",
-		})
-	end)
-	local snap_shot_time = os.date("!%Y-%m-%dT%TZ")
-	vim.cmd("PackerSnapshot " .. snap_shot_time)
-	vim.cmd("PackerSync")
-end
-
-vim.keymap.set("n", "<leader>ps", function() packer_sync() end)
