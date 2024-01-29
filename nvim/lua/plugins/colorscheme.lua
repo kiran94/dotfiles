a646f6a2cf1590a3d61c95596d438934c8fc397b
kiran94 <kiran_patel94@hotmail.com>
2024-01-29 22:06:57 +0000

    chore(nvim): add colorschemes

---
+++ nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,32 @@
+return {
+	{
+		"Mofiqul/vscode.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"rose-pine/neovim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"tomasiser/vim-code-dark",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"lighthaus-theme/vim-lighthaus",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"projekt0n/github-nvim-theme",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+}
