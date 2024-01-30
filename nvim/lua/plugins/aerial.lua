5cd4d75530a0c62b0bc411d75905abe15ddb93ee
kiran94 <kiran_patel94@hotmail.com>
2024-01-30 21:47:47 +0000

    refactor(nvim): add min_width to aerial

--- nvim/lua/plugins/aerial.lua
+++ nvim/lua/plugins/aerial.lua
@@ -6,6 +6,9 @@ return {
 			vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
 			vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
 		end,
+		layout = {
+			min_width = 40,
+		},
 	},
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
