3d28d1d5c5ebdff9b37144ecf9513a46141d9eac
kiran94 <kiran_patel94@hotmail.com>
2024-01-30 17:56:22 +0000

    chore(nvim): remove warning

--- nvim/lua/plugins/lsp/dap-python.lua
+++ nvim/lua/plugins/lsp/dap-python.lua
@@ -3,7 +3,6 @@ local M = {}
 M.setup = function(dap, install_directory)
 	local dap_python_ok, dap_python = pcall(require, "dap-python")
 	if dap_python_ok then
-		-- WARN: this is not working
 		local p = vim.fn.stdpath("data") .. "/mason/packages/debugpy/venv/bin/python3"
 		dap_python.setup(p)
 		dap_python.test_runner = "pytest"
