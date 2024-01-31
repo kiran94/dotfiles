d904f7ba9f41e1dd38b3cf370698473fd03f1410
kiran94 <kiran_patel94@hotmail.com>
2024-01-31 09:58:46 +0000

    fix(nvim): bash debugging

--- nvim/lua/plugins/lsp/dap-bash.lua
+++ nvim/lua/plugins/lsp/dap-bash.lua
@@ -1,41 +1,42 @@
 local M = {}
 
 M.setup = function(_, install_directory)
-    local mason_registry = require("mason-registry")
-    local dap = require("dap")
+	local mason_registry = require("mason-registry")
+	local dap = require("dap")
 
-	if mason_registry.is_installed("bash-debug-adapter") then
-		dap.adapters.sh = {
-			type = "executable",
-			command = install_directory .. "bash-debug-adapter",
-			args = { "--interpreter=vscode" },
-		}
-
-		dap.configurations.sh = {
-			{
-				args = {},
-				argsString = "",
-				cwd = vim.fn.getcwd(),
-				env = {},
-				name = "Launch",
-				pathBash = "/usr/bin/bash",
-				-- bash-debug-adapter
-				-- https://sourceforge.net/projects/bashdb/
-                -- TODO: Get this installed and make OS dynamic
-				-- pathBashdb = "/opt/homebrew/bin/bashdb",
-				-- pathBashdbLib = "/opt/homebrew/share/bashdb/",
-				pathCat = "/bin/cat",
-				pathMkfifo = "/usr/bin/mkfifo",
-				pathPkill = "/usr/bin/pkill",
-				program = "${file}",
-				request = "launch",
-				showDebugOutput = false,
-				terminalKind = "integrated",
-				trace = true,
-				type = "sh",
-			},
-		}
+	if not mason_registry.is_installed("bash-debug-adapter") then
+		return
 	end
+
+	local mason_directory = vim.fn.stdpath("data") .. "/mason/"
+	local mason_bin_directory = mason_directory .. "bin/"
+	local bashdb_directory = mason_directory .. "packages/bash-debug-adapter/extension/bashdb_dir"
+
+	dap.adapters.sh = {
+		type = "executable",
+		command = mason_bin_directory .. "bash-debug-adapter",
+		args = { "--interpreter=vscode" },
+	}
+
+	dap.configurations.sh = {
+		{
+			name = "Launch Bash Debugger",
+			type = "sh",
+			request = "launch",
+			program = "${file}",
+			cwd = "${fileDirname}",
+			pathBashdb = bashdb_directory .. "/bashdb",
+			pathBashdbLib = bashdb_directory,
+			pathBash = "bash",
+			pathCat = "cat",
+			pathMkfifo = "mkfifo",
+			pathPkill = "pkill",
+			env = {},
+			args = {},
+			-- showDebugOutput = true,
+			-- trace = true,
+		},
+	}
 end
 
 return M
