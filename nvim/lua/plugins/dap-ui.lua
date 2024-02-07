8e711a89f2c9a0d2a8a99b59c167045dcdaf6aa4
kiran94 <kiran_patel94@hotmail.com>
2024-02-07 21:46:37 +0000

    refactor(nvim): update dap ui open and close

--- nvim/lua/plugins/dap-ui.lua
+++ nvim/lua/plugins/dap-ui.lua
@@ -9,12 +9,19 @@ return {
 
 		dap.listeners.after.event_initialized["dapui_config"] = function()
 			dapui.open({})
+			dapui.toggle({ layout = 4 })
+
+			vim.cmd(":FocusDisable")
 		end
 		dap.listeners.before.event_terminated["dapui_config"] = function()
 			dapui.close({})
+
+			vim.cmd(":FocusEnable")
 		end
 		dap.listeners.before.event_exited["dapui_config"] = function()
 			dapui.close({})
+
+			vim.cmd(":FocusEnable")
 		end
 	end,
 	dependencies = {
