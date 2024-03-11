4404a93068effc588465036a949b68ea2f7753f5
kiran94 <kiran_patel94@hotmail.com>
2024-03-11 08:13:23 +0000

    refactor(nvim): consolidated deprecated formatters
    https://github.com/nvimtools/none-ls.nvim/issues/58

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -9,45 +9,18 @@ return {
 
 		null_ls.setup({
 			sources = {
-				-- Python
-				-- formatting.autopep8,
-				-- formatting.isort,
-				-- diagnostics.flake8,
-				-- diagnostics.pyproject_flake8,
-				-- diagnostics.ruff,
-				formatting.ruff,
-
-				-- Go
 				formatting.gofmt,
 				formatting.goimports,
-				-- formatting.golines,
-				actions.gomodifytags,
-				diagnostics.staticcheck,
-
-				-- Lua
-				-- formatting.lua_format,
 				formatting.stylua,
-
-				-- Others
 				formatting.terraform_fmt,
 				formatting.yamlfmt,
 				formatting.csharpier,
-				formatting.beautysh,
-				diagnostics.yamllint,
-				actions.refactoring,
+				formatting.shfmt,
 				formatting.clang_format.with({ filetypes = { "c", "cpp", "java", "cuda", "proto" } }),
-
-				-- diagnostics.cspell.with({ filetypes = { "markdown", "text" } }),
-				-- actions.cspell.with({ filetypes = { "markdown", "text" } }),
-
-				-- JavaScript
-				-- formatting.eslint,
-				-- formatting.prettier,
-				-- formatting.json_tool,
-				formatting.jq,
-
-				-- Rust
-				formatting.rustfmt,
+				diagnostics.staticcheck,
+				diagnostics.yamllint,
+				actions.gomodifytags,
+				actions.refactoring,
 			},
 			on_attach = function()
 				-- NOTE: This is duplicated with lsp-installer
