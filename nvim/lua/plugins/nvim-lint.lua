ea852144e7a89c89066c2465155eace814f926a3
kiran94 <kiran_patel94@hotmail.com>
2024-03-28 21:02:12 +0000

    feat(nvim): integrate nvim-lint

---
+++ nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,34 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = {
+		"BufReadPre",
+		"BufNewFile",
+	},
+	config = function()
+		local lint = require("lint")
+
+		lint.linters_by_ft = {
+			lua = { "luacheck" },
+			json = { "jsonlint" },
+			sql = { "sqlfluff" },
+			sh = { "shellcheck" },
+			terraform = { "terraform_validate" },
+		}
+
+		local function has_bin(bin)
+			return vim.fn.executable(bin) == 1
+		end
+
+		if has_bin("ruff") then
+			lint.linters_by_ft.python = { "ruff" }
+		elseif has_bin("flake8") then
+			lint.linters_by_ft.python = { "flake8" }
+		end
+
+		vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			callback = function()
+				require("lint").try_lint()
+			end,
+		})
+	end,
+}
