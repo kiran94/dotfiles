594767c94dcec80a6e9fba043ab5850bee2c375c
kiran94 <kiran_patel94@hotmail.com>
2024-04-27 11:04:02 +0100

    feat(nvim): add dap snippets

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -15,5 +15,65 @@ return {
 		vim.api.nvim_set_keymap("s", "<c-j>", "<cmd>lua require'luasnip'.jump(1)<CR>", opts)
 		vim.api.nvim_set_keymap("i", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
 		vim.api.nvim_set_keymap("s", "<c-k>", "<cmd>lua require'luasnip'.jump(-1)<CR>", opts)
+
+		local ls = require("luasnip")
+		local s = ls.snippet
+		local t = ls.text_node
+		local i = ls.insert_node
+		local fmt = require("luasnip.extras.fmt").fmt
+		local rep = require("luasnip.extras").rep
+
+		ls.add_snippets("json", {
+			s(
+				"debug_start",
+				fmt(
+					[[
+                {{
+                    "version": "0.2.0",
+                    "configurations": [{}]
+                }}
+                ]],
+					{ i(0) }
+				)
+			),
+			s(
+				"debug_python_file",
+				fmt(
+					[[
+                {{
+                  "name": "Python: Current File",
+                  "type": "python",
+                  "request": "launch",
+                  "module": "${{file}}",
+                  "console": "integratedTerminal",
+                  "justMyCode": true,
+                  "envFile": "${{workspaceFolder}}/.env",
+                  "env": {{}}
+                  "args": {{}}
+                }}
+                ]],
+					{}
+				)
+			),
+			s(
+				"debug_python_module",
+				fmt(
+					[[
+                {{
+                    "name": "Python: Module",
+                    "type": "python",
+                    "request": "launch",
+                    "module": "{}",
+                    "console": "integratedTerminal",
+                    "justMyCode": true,
+                    "envFile": "${{workspaceFolder}}/.env",
+                    "env": {{}}
+                    "args": {{}}
+                }}
+                ]],
+					{ i(0, "module") }
+				)
+			),
+		})
 	end,
 }
