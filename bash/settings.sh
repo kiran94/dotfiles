394cfe7c85463b40c0c19bad223bd3ac4b3fcae9
kiran94 <kiran_patel94@hotmail.com>
2024-07-10 22:28:31 +0100

    fix(bash): add tz list override

--- bash/settings.sh
+++ bash/settings.sh
@@ -65,3 +65,6 @@ HISTTIMEFORMAT="%F %T "
 
 # colored GCC warnings and errors
 export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# timezone settings for https://github.com/oz/tz
+export TZ_LIST="America/New_York;Asia/Calcutta;Asia/Manila;Asia/Tokyo"
